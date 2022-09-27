c78137e00486997b35596d5e6e5781595afdee5e
Alexey Ivanenko <alexey.i@legalbet.com>
2022-09-27 15:20:25 +0300

    categories list endpoint

--- actions/quiz.go
+++ actions/quiz.go
@@ -2,11 +2,19 @@ package actions
 
 import (
 	"net/http"
+	"tic-tac-toe__api/models"
 
 	"github.com/gobuffalo/buffalo"
 )
 
 // QuizCategories default implementation.
 func QuizCategories(c buffalo.Context) error {
-	return c.Render(http.StatusOK, r.JSON(map[string]string{"message": "Categories list"}))
+	categories := models.Categories{}
+	err := models.DB.Where("published = true").All(&categories)
+
+	if err != nil {
+		return c.Render(http.StatusOK, r.JSON(err))
+	}
+
+	return c.Render(http.StatusOK, r.JSON(categories))
 }
---
+++ models/category.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"encoding/json"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+)
+
+// Category is used by pop to map your categories database table to your go code.
+type Category struct {
+	ID        int64  `json:"id" db:"id"`
+	Name      string `json:"name" db:"name"`
+	Published bool   `json:"published" db:"published"`
+	Hash      string `json:"hash" db:"hash"`
+}
+
+// String is not required by pop and may be deleted
+func (c Category) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Categories is not required by pop and may be deleted
+type Categories []Category
+
+// String is not required by pop and may be deleted
+func (c Categories) String() string {
+	jc, _ := json.Marshal(c)
+	return string(jc)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (c *Category) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (c *Category) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (c *Category) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
